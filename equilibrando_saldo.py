f6ee91e96e27cc4a3a146459c773897392dc21ad
Camily Assumpção <141678971+camily-assumpcao@users.noreply.github.com>
2024-11-26 15:54:59 -0300

    equilibrando_saldo.py

--- equilibrando_saldo.py
+++ equilibrando_saldo.py
@@ -1,3 +1,11 @@
+"""
+Para esse desafio, considere que você foi contratado por uma empresa bancária para auxiliar nas implementações e melhorias do sistema 
+empresarial. Em uma análise inicial, foi identificado pela equipe financeira a necessidade de desenvolver uma solução que permita ao 
+cliente equilibrar seu saldo bancário. Dessa forma, o programa deve solicitar uma entrada que representa o saldo atual do funcionário, 
+e após, seja informado o valor de duas transações, sendo elas: um depósito e um saque. O programa deve atualizar o saldo com base nas 
+transações e exibir o saldo final.
+"""
+
 # Número de iterações
 num_transacoes = int(input("Quantas transações deseja processar? "))
 
